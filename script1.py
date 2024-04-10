e89c3269eb808272a233de708cac548f83bb54e2
silviamh02 <silviamh02@gmail.com>
2024-04-10 14:47:12 +0200

    Clase manejadora de eventos + main function

--- script1.py
+++ script1.py
@@ -11,13 +11,6 @@ from watchdog.events import FileSystemEventHandler
 def parse_config_file(config_file_path):
     with open(config_file_path, 'r') as config_file: # Abre el archivo en modo lectura
         config_data = json.load(config_file) # Carga el JSON en un diccionario
-    
-    # # Extraer las variables necesarias del archivo de configuración
-    # topology_path = config_data.get('topology_path')
-    # behavior_path = config_data.get('behavior_path')
-    # mqtt_broker_ip = config_data.get('mqtt_broker_ip')
-    # mqtt_broker_port = config_data.get('mqtt_broker_port')
-    
     return config_data # Devuelve el diccionario
 
 
@@ -90,3 +83,72 @@ def generate_behavior_json(behavior_path):
     # Devolver la ruta completa del archivo generado
     return file_path
 
+
+# Clase para manejar los eventos del sistema de archivos
+class MyFileSystemEventHandler(FileSystemEventHandler):
+    # Constructor de la clase
+    def __init__(self, mqtt_client, mqtt_topic): 
+    #def __init__(self, mqtt_client, mqtt_topic, state): 
+        super().__init__() # Llama al constructor de la clase padre
+        self.mqtt_client = mqtt_client # Inicializa el cliente MQTT
+        self.mqtt_topic = mqtt_topic # Inicializa el topic MQTT
+        #self.state = state # Inicializa el estado del agente
+        
+    # Función para manejar el evento de creación de archivos
+    def on_created(self, event): 
+        if event.is_directory: # Verifica si el evento es un directorio
+            return # Si es un directorio, no hace nada
+        filepath = event.src_path # Obtiene la ruta del archivo creado
+        print(f"Nuevo archivo creado: {filepath}") # Imprime un mensaje en consola
+        
+        # Primera prueba SIN VERIFICACIÓN DE ESTADO
+        self.publish_json(filepath) # Llama al método para publicar el contenido del archivo JSON
+
+        # # Verifica el estado del agente
+        # if self.state == 0:
+        #     self.publish_json(filepath) # Llama al método para publicar el contenido del archivo JSON
+
+    # Función para publicar el contenido del archivo JSON
+    def publish_json(self, filepath): 
+        # Publicar el contenido del JSON a Elasticsearch vía MQTT
+        with open(filepath, 'r') as json_file: # Abre el archivo en modo lectura
+            json_content = json.load(json_file) # Carga el contenido JSON en un diccionario
+            self.mqtt_client.publish(self.mqtt_topic, json.dumps(json_content)) # Publica el contenido JSON en el topic MQTT
+
+   
+# Main function
+def main():
+    # Parsear el archivo de configuración
+    config = parse_config_file("config.json")
+
+    # Configurar las variables
+    topology_path = config["topology_path"]
+    #behavior_path = config["behavior_path"]
+    #mgmt_path = config["mgmt_path"]
+    
+    mqtt_broker_ip = config["mqtt_broker_ip"]
+    mqtt_broker_port = config["mqtt_broker_port"]
+    
+    # Iniciar el cliente MQTT
+    mqtt_client = mqtt.Client()
+    mqtt_client.connect(mqtt_broker_ip, mqtt_broker_port)
+
+    # # Verificar el estado del agente
+    # agent_state_verifier(mgmt_path, mqtt_client)
+
+    observer = Observer()
+    event_handler = MyFileSystemEventHandler(mqtt_client, "TOPOLOGY") 
+    observer.schedule(event_handler, topology_path, recursive=True) 
+    observer.start() 
+    print(f"Watchdog iniciado para el directorio: {topology_path}") 
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+# Ejecutar la función principal
+if __name__ == "__main__":
+    main()
